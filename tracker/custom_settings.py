07fa74377742622431f9f10de85cfb02026b9607
Brendan Kamp <brendankamp757@gmail.com>
2017-09-04 07:10:34 +0200

    Added static routing

--- tracker/custom_settings.py
+++ tracker/custom_settings.py
@@ -17,3 +17,5 @@ AUTHENTICATION_BACKENDS = [
 MEDIA_URL = '/media/'
 STATIC_ROOT = './static/'
 STATIC_URL = '/static/'
+if os.environ.get('ENVIRONMENT') == 'production':
+    STATIC_URL = 'http://127.0.0.1:7072:/static/'
